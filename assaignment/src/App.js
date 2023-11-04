dc843c34429d63056c7705aaf8961002a1fd1d21
SuryaSatyaNikhil503 <21a95a0503@aec.edu.in>
2023-11-04 21:12:18 +0530

    Second Commit

--- assaignment/src/App.js
+++ assaignment/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+import Task from './Task';
+import TaskForm from './TaskForm';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, { ...newTask, id: Date.now(), completed: false }]);
+  }
+
+  const toggleComplete = (taskId) => {
+    const updatedTasks = tasks.map(task =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  }
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="mb-4">Task Manager</h1>
+      <div className="row mb-4">
+        <div className="col-md-6">
+          <TaskForm onSubmit={addTask} />
+        </div>
+      </div>
+      {tasks.map(task =>
+        <Task
+          key={task.id}
+          task={task}
+          onToggle={toggleComplete}
+          onDelete={deleteTask}
+        />
+      )}
     </div>
   );
 }
